39e3dd70e0abd3c832b16cef03989e43e7b99f0b
furkanGit <furkancaliskan9801@gmail.com>
2022-12-12 17:38:48 +0100

    Create Component AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: "AppHeader",
+
+}
+
+</script>
+
+<template>
+    <header>
+        <a href="/" class="brand">
+           <img src="../assets/images/dc-logo.png" alt="" /> 
+        </a>
+    </header>
+ 
+</template>
+
+<style lang="scss" scoped>
+    header{
+        text-align: start;
+    }
+</style>
